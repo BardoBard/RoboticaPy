95dd88b24743ec9bc388806fe4ff975e6cb0bfcd
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-02 15:37:48 +0200

    forgor self

--- Components/Internal/Bluetooth.py
+++ Components/Internal/Bluetooth.py
@@ -10,7 +10,7 @@ class Bluetooth:
         for addr, name in nearby_devices: 
             print(" %s - %s" % (addr, name))
             
-    def connect(mac_addresss):
+    def connect(self, mac_addresss):
         service_matches = find_service(mac_addresss)
         print("found %d services" % len(service_matches))
         for i in range(len(service_matches)):

4f146a46aab3befa0905ffe246b9bedd1692878d
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-02 16:16:12 +0200

    fix string formatting

--- Components/Internal/Bluetooth.py
+++ Components/Internal/Bluetooth.py
@@ -19,9 +19,9 @@ class Bluetooth:
             return
         
         for i in range(len(service_matches)):
-            print("name: %d" % service_matches[i]["name"])
-            print("port: %d" % service_matches[i]["port"])
-            print("protocol: %d" % service_matches[i]["protocol"])
+            print("name: " + service_matches[i]["name"])
+            print("port: " + service_matches[i]["port"])
+            print("protocol: " + service_matches[i]["protocol"])
         
         if(len(service_matches) == 1):
             print("connecting")

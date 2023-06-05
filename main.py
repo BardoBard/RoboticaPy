16f8bec7e7982b790bfaf2f5fc4ed0b7598e6e49
Bard <nigel.terpstra@student.nhlstenden.com>
2023-06-05 16:14:12 +0200

    test disconnect with app

--- main.py
+++ main.py
@@ -19,14 +19,19 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi(':)')
     controller_mac_address = "78:21:84:7C:A4:F6"  # controller_mac_address
-    # app_mac_address = "00:E1:8C:A5:60:44"  # app_mac_address
-    # Bluetooth.scan()
+    app_mac_address = "00:E1:8C:A5:60:44"  # app_mac_address
+    Bluetooth.scan()
 
     socket = Bluetooth.connect(controller_mac_address)
-    # socket = Bluetooth.connect(app_mac_address, "APP")
+    socket2 = Bluetooth.connect(app_mac_address, "APP")
 
-    t1 = threading.Thread(get_controller_data(socket))
-    t1.start()
+    socket2.send("hello world")
+    Bluetooth.disconnect(socket2)
+    socket2 = Bluetooth.connect(app_mac_address, "APP")
+    socket2.send("hello world2")
+    Bluetooth.disconnect(socket2)
+
+    threading.Thread.daemon(get_controller_data(socket))
+
     while True:
         print("hello")
-    t1.join()

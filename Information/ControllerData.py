17da2a137c662da757a25218f1344b370202fbf5
Bard <nigel.terpstra@student.nhlstenden.com>
2023-06-06 10:31:01 +0200

    testing normalization

--- Information/ControllerData.py
+++ Information/ControllerData.py
@@ -50,5 +50,7 @@ class ControllerData:
     @staticmethod
     def normalize():
         sum_joystick = ControllerData.joystick1[0] + ControllerData.joystick1[1] - (700 + 700)
-        new_matrix = ControllerData.joystick1 / sum_joystick[:, numpy.newaxis]
-        print(new_matrix)
+        ControllerData.joystick1[0] /= sum_joystick
+        ControllerData.joystick1[1] /= sum_joystick
+        print("joystick1 x: ", ControllerData.joystick1[0])
+        print("joystick1 y: ", ControllerData.joystick1[1])

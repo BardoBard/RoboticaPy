715e9b2c9565b6df2f7941fd918187fa3b284b21
Bard <nigel.terpstra@student.nhlstenden.com>
2023-06-06 15:33:11 +0200

    testing

--- Components/Internal/TrackMotor.py
+++ Components/Internal/TrackMotor.py
@@ -11,7 +11,7 @@ class TrackMotor:
         # ControllerData.normalize()
         byte_arr = bytearray(
             [(int(abs(ControllerData.joystick1[0]) * 255)).to_bytes(1, 'little'), bool(ControllerData.joystick1[0] > 0),
-             (int(abs(ControllerData.joystick1[1]) * 255)).to_bytes(1, 'little'), bool(ControllerData.joystick1[1] > 0]))
+             (int(abs(ControllerData.joystick1[1]) * 255)).to_bytes(1, 'little'), bool(ControllerData.joystick1[1] > 0)])
 
         print(len(byte_arr))
 

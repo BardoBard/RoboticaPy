f83a4d364fbd40c33545ee3e46118c98b819f6aa
Bard <nigel.terpstra@student.nhlstenden.com>
2023-06-06 11:09:52 +0200

    normalize -1 and 1

--- Components/Math.py
+++ Components/Math.py
@@ -6,3 +6,7 @@ class Math:
     @staticmethod
     def normalize(value, min_value, max_value):
         return (value - min_value) / (max_value - min_value)
+
+    @staticmethod
+    def normalize_neg_one(value, min_value, max_value):
+        return 2((value - min_value) / (max_value - min_value)) - 1

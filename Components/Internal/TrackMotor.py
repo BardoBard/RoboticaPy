6d1f058b4a118fc474f093a1e89cf06cfc9904f7
Bard <nigel.terpstra@student.nhlstenden.com>
2023-06-06 15:37:16 +0200

    testing

--- Components/Internal/TrackMotor.py
+++ Components/Internal/TrackMotor.py
@@ -9,11 +9,12 @@ class TrackMotor:
     def activate_motor():
         ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)  # TODO: change usb to config file
         # ControllerData.normalize()
-        byte_arr = [(int(abs(ControllerData.joystick1[0]) * 255)).to_bytes(1, 'little'),
-                    bool(ControllerData.joystick1[0] > 0),
-                    (int(abs(ControllerData.joystick1[1]) * 255)).to_bytes(1, 'little'),
-                    bool(ControllerData.joystick1[1] > 0)]
+        byte_arr = bytearray([0, 0, 0, 0])
 
+        byte_arr[0] = (int(abs(ControllerData.joystick1[0]) * 255)).to_bytes(1, 'little')
+        byte_arr[1] = bool(ControllerData.joystick1[0] > 0)
+        byte_arr[2] = (int(abs(ControllerData.joystick1[1]) * 255)).to_bytes(1, 'little')
+        byte_arr[3] = bool(ControllerData.joystick1[1] > 0)
         print(len(byte_arr))
 
         ser.write(byte_arr)

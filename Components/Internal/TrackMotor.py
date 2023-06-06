baf7d22be564acadf7385b04713f4e273b3baa5e
Bard <nigel.terpstra@student.nhlstenden.com>
2023-06-06 16:45:58 +0200

    write

--- Components/Internal/TrackMotor.py
+++ Components/Internal/TrackMotor.py
@@ -16,7 +16,8 @@ class TrackMotor:
         byte_arr.append(bool(ControllerData.joystick1[0] > 0))
         byte_arr.append(int(abs(ControllerData.joystick1[1]) * 255))
         byte_arr.append(bool(ControllerData.joystick1[1] > 0))
-        print(byte_arr)
+        for i in byte_arr:
+            print(int(i))
         print(len(byte_arr))
 
         TrackMotor.ser.write(byte_arr)

18117af7fa0b15877d9731d7880234f8610f3aa8
Bard <nigel.terpstra@student.nhlstenden.com>
2023-06-06 13:12:53 +0200

    changed usb

--- Components/Internal/TrackMotor.py
+++ Components/Internal/TrackMotor.py
@@ -5,7 +5,7 @@ import serial as serial
 class TrackMotor:
     @staticmethod
     def activate_motor():
-        ser = serial.Serial(port='/dev/ttyGS0', baudrate=115200)
+        ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
         ser.write("hi")
         ser.close()
         return  # void

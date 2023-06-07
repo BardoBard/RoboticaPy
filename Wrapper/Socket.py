254ef591b0f673f54bd214012615bd987af9a223
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-07 13:22:53 +0200

    socket wrapper test

--- Wrapper/Socket.py
+++ Wrapper/Socket.py
@@ -4,7 +4,7 @@ from Components.Internal.Bluetooth import Bluetooth
 
 
 class Socket:
-    __socket = False
+    socket = False
     __address = None
     __name = None
 
@@ -14,27 +14,27 @@ class Socket:
             self.check_connection()  # Check the connection before accessing _socket
 
     def check_connection(self):
-        while self.__socket is None:
+        while self.socket is None:
             print("connection lost, reconnecting in 1 second")
             time.sleep(1)
-            self.__socket = Bluetooth.connect(self._address, self._name)
+            self.socket = Bluetooth.connect(self._address, self._name)
 
     def __init__(self, address: str, name: str = None):
-        self._address = address
-        self._name = name
-        self.__socket = Bluetooth.connect(address, name)
-        print(self.__socket is None)
+        self.__address = address
+        self.__name = name
+        self.socket = Bluetooth.connect(address, name)
+        print(self.socket is None)
 
     # def __del__(self):
     #     self._socket.close()
 
     def send(self, message):
         print("printing socket...")
-        print(self.__socket)
+        print(self.socket)
         # self._socket.send(message)
 
     def receive(self, size):
-        self.__socket.recv(size)
+        self.socket.recv(size)
 
     def close(self, size):
-        self.__socket.close()
+        self.socket.close()

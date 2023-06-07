e7a0d30272d1cd4e706b58b7c711811d628d60de
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-07 14:37:44 +0200

    socket wrapper test

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from Information.ControllerData import ControllerData
 from Wrapper.Socket import Socket
 
 
-def get_controller_data(bluetooth_socket):
+async def get_controller_data(bluetooth_socket):
     while True:
         data = bluetooth_socket.receive(14)
         ControllerData.fill_data(data)
-        # TrackMotor.activate_motor()
+        TrackMotor.activate_motor()
 
 
 def print_hi(name):
@@ -25,17 +25,17 @@ if __name__ == '__main__':
     # Bluetooth.scan()
 
     socket = Socket(controller_mac_address)
-    print(socket.address)
-    print(socket.socket)
-    while True:
-        print(socket.receive(14))
-    socket.close()
+    # print(socket.address)
+    # print(socket.socket)
+    # while True:
+    #     print(socket.receive(14))
+    # socket.close()
     # socket2 = Bluetooth.connect(app_mac_address, "APP") #TODO: make sure application doesn't crash
 
     # socket2.send("hello world")
     # Bluetooth.disconnect(socket2)
 
-    # threading.Thread.daemon(get_controller_data(Socket))  # TODO: fix daemon thread
+    get_controller_data(Socket)  # TODO: fix daemon thread
 
     # while True:
     #     print("hello")

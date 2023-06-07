49d3d56335323aeebac642f655a228d34516d341
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-07 14:44:29 +0200

    socket wrapper test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Information.ControllerData import ControllerData
 from Wrapper.Socket import Socket
 
 
-async def get_controller_data(bluetooth_socket):
+def get_controller_data(bluetooth_socket):
     while True:
         data = bluetooth_socket.receive(14)
         ControllerData.fill_data(data)
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     # socket2.send("hello world")
     # Bluetooth.disconnect(socket2)
 
-    asyncio.run(get_controller_data(Socket))  # TODO: fix daemon thread
+    get_controller_data(Socket)  # TODO: fix daemon thread
 
     # while True:
     #     print("hello")

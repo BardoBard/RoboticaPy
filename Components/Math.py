0a53dfe2db9eb190282b2861814217ff952dc27c
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-07 11:53:50 +0200

    added clamp

--- Components/Math.py
+++ Components/Math.py
@@ -11,11 +11,11 @@ class Math:
         """
         normalizes a number between 0 and 1
         """
-        return numpy.clip((value - min_value) / (max_value - min_value), min=-1, max=1)
+        return numpy.clip((value - min_value) / (max_value - min_value), a_min=-1, a_max=1)
 
     @staticmethod
     def normalize_neg(value, min_value, max_value):
         """
         normalizes a number between -1 and 1
         """
-        return numpy.clip(2 * ((value - min_value) / (max_value - min_value)) - 1, min=-1, max=1)
+        return numpy.clip(2 * ((value - min_value) / (max_value - min_value)) - 1, a_min=-1, a_max=1)

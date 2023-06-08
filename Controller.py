07afa81558e46e7bd9d291af7ef666f72625e756
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-08 10:51:58 +0200

    removes the erroniously added main.py while preserving a couple important constants

--- Controller.py
+++ Controller.py
@@ -1,12 +1,4 @@
 class Controller:
-    def remote_to_arm(self, value):
-        return  # void
-
-    def weight_to_text(self, weight):  # not sure what this does, perhaps not necessary
-        return  # void
-
-    def turn_led_on(self, frequency):
-        return  # void
-
-    def detect_object(self):
-        return  # void
+    controller_mac_address = "78:21:84:7C:A4:F6"  # controller_mac_address
+    app_mac_address = "00:E1:8C:A5:60:44"  # app_mac_address
+    app_service_name = "APP"

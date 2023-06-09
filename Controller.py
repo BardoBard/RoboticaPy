5e277df4812f8363980d38c10b8d0950eb6dab93
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-09 10:43:54 +0200

    image detection in it's own process

--- Controller.py
+++ Controller.py
@@ -8,10 +8,8 @@ def detection_process(queue):
     opencv_ = OpenCv()
     
     while True:
-        #print("loop 2")
         image_data = opencv_.get_image_date_from_feed()
         if image_data.found:
-            #print("detected something!")
             image_data = scan_data_matrix(image_data)
             queue.put(image_data)
                 
@@ -20,23 +18,22 @@ class Controller:
     #controller_mac_address = "78:21:84:7C:A4:F6"  # controller_mac_address
     #app_mac_address = "00:E1:8C:A5:60:44"  # app_mac_address
     #app_service_name = "APP"
-    
-    
-                
+              
     if __name__ == '__main__':
         queue = Queue()
         process = Process(target=detection_process, args=(queue, ))
-        process.start()
+        process.start() # start the image detection program
         
-        #while True:
-            #print("loop1")
-        time.sleep(15)
+        time.sleep(15) # capture images for 15 seconds
+        
+        process.kill() # stop the image detection code
+        
         while queue.qsize() > 1:
             obj = queue.get()
             print(type(obj))
-            obj.print_to_command_line()
+            obj.print_to_command_line() # then print the image data to the console
             
-        process.kill()   
+        
             
         
         

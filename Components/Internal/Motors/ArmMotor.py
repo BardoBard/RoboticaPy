bc4b08ba6e6d02338f0107af9b0e7a1db3a5eb20
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-12 10:57:57 +0200

    testing

--- Components/Internal/Motors/ArmMotor.py
+++ Components/Internal/Motors/ArmMotor.py
@@ -10,10 +10,10 @@ from Components.Internal.Motors.Ax12 import Ax12
 class ArmMotor:
     @staticmethod
     def initialize():
-        Ax12.connect()
-
         Ax12.DEVICENAME = '/dev/ttyAMA0'
         Ax12.BAUDRATE = 1_000_000
+
+        Ax12.connect()
 
         my_dxl = Ax12(254)
         my_dxl.set_moving_speed(200)

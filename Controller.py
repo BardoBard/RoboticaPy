e2164878526fbda49eb6adc3e0b1683fb249ba58
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-12 12:53:07 +0200

    cleaned up debug statements

--- Controller.py
+++ Controller.py
@@ -16,7 +16,7 @@ def detection_process(queue):
             image_data = scan_data_matrix(image_data)
             queue.send_message(QueueAgent.OPENCV, QueueAgent.CONTROLL, image_data)
                 
-                
+          
 class Controller:
     #controller_mac_address = "78:21:84:7C:A4:F6"  # controller_mac_address
     #app_mac_address = "00:E1:8C:A5:60:44"  # app_mac_address
--- Wrapper/MessageQueue.py
+++ Wrapper/MessageQueue.py
@@ -4,23 +4,23 @@ from Information.QueueMessage import QueueMessage
 class MessageQueue:
     def __init__(self):
         self.__queue = Queue()
-        print("queue instantiated")
+        
+    def exit_queue(self):
+        self.__queue.close()
         
     def get_queue(self):
         return self.__queue
     
     def get_messages_for(self, queue_agent):
-        print("getting messages")
         if self.__queue.empty():
-            print("queue was empty")
             return None
         
         all_messages = []
-        #get all the messages from the queue
+        #Get all the messages from the queue
         while self.__queue.qsize() > 0:
-            print("queue size is: {}".format( self.__queue.qsize()))
+            # WARNING: This will hang if one of the processes is killed without calling exit queue from it
+            # It 
             all_messages.append(self.__queue.get())
-            print("queue size is: {}".format( self.__queue.qsize()))
         
         return_messages = []
         unreturned_messages = []
@@ -37,5 +37,4 @@ class MessageQueue:
         return return_messages
         
     def send_message(self, sender, recipient, obj):
-        print("sending message from {} to {} ".format(sender, recipient))
         self.__queue.put(QueueMessage(sender, recipient, obj))

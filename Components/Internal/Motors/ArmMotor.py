e764a28f6f7b8821389effba1c327368a7b25f23
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-12 10:29:01 +0200

    testing

--- Components/Internal/Motors/ArmMotor.py
+++ Components/Internal/Motors/ArmMotor.py
@@ -3,33 +3,26 @@ import time
 import RPi.GPIO as GPIO
 import serial
 from pyax12 import connection
+import dynamixel_sdk
+
+from Components.Internal.Motors.Ax12 import Ax12
 
 
 class ArmMotor:
     @staticmethod
     def initialize():
+        Ax12.DEVICENAME = 'ttyAMA0'
+        Ax12.BAUDRATE = 1_000_000
+
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(18, GPIO.OUT)
         GPIO.output(18, GPIO.HIGH)
+        Ax12.connect()
+        my_dxl = Ax12(254)
+        my_dxl.set_moving_speed(200)
 
-        servo_id = 254
-        # serial_connection = connection.Connection(port="/dev/ttyAMA0", baudrate=1000000, timeout=3.0)
-        port = serial.Serial(port="/dev/ttyAMA0", baudrate=1000000, timeout=3.0)
-        print("scanning...")
+        my_dxl.set_goal_position(500)
 
-        while True:
-            GPIO.output(18, GPIO.HIGH)
-            time.sleep(0.1)
-            port.write(bytearray.fromhex("FF FF 01 05 03 1E CD 00 Fb"))
-            GPIO.output(18, GPIO.LOW)
-            time.sleep(3)
-
-            GPIO.output(18, GPIO.HIGH)
-            time.sleep(0.1)
-            port.write(bytearray.fromhex("FF FF 01 05 03 1E CD 00 0b"))
-            time.sleep(3)
-        # serial_connection.goto(servo_id, 45, speed=200, degrees=True)
-
-        print("closing")
-        # serial_connection.close()
+        my_dxl.set_torque_enable(0)
+        Ax12.disconnect()

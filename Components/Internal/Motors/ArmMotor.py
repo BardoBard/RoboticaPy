cca0609b89a63354a62ad8e4249322d874b25376
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-12 11:07:33 +0200

    testing

--- Components/Internal/Motors/ArmMotor.py
+++ Components/Internal/Motors/ArmMotor.py
@@ -10,7 +10,7 @@ from Components.Internal.Motors.Ax12 import Ax12
 class ArmMotor:
     @staticmethod
     def initialize():
-        Ax12.DEVICENAME = '/dev/ttyAMA0'
+        Ax12.DEVICENAME = '/dev/tty14'
         Ax12.BAUDRATE = 1_000_000
 
         Ax12.connect()
@@ -20,8 +20,8 @@ class ArmMotor:
 
         my_dxl.set_goal_position(500)
 
-        # GPIO.setmode(GPIO.BCM)
-        # GPIO.setup(18, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.OUT)
         GPIO.output(18, GPIO.HIGH)
 
         # disconnect

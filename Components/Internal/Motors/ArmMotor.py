099a3f938d884267140264f1ed7874a62d09876f
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-12 10:40:02 +0200

    testing

--- Components/Internal/Motors/ArmMotor.py
+++ Components/Internal/Motors/ArmMotor.py
@@ -11,14 +11,18 @@ from Components.Internal.Motors.Ax12 import Ax12
 class ArmMotor:
     @staticmethod
     def initialize():
-        Ax12.DEVICENAME = '/dev/ttyAMA0'
-        Ax12.BAUDRATE = 1_000_000
+        # Ax12.DEVICENAME = '/dev/ttyAMA0'
+        # Ax12.BAUDRATE = 1_000_000
 
-        GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(18, GPIO.OUT)
         GPIO.output(18, GPIO.HIGH)
+
+
         Ax12.connect()
+
+        # Ax12.
+
         my_dxl = Ax12(254)
         my_dxl.set_moving_speed(200)
 

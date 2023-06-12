93b072199a53a7d8901198eb7c7f6db7c6f52e67
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-12 15:29:58 +0200

    read values

--- Information/ControllerData.py
+++ Information/ControllerData.py
@@ -39,16 +39,16 @@ class ControllerData:
             ControllerData.RA = bool(data[12])
             ControllerData.RB = bool(data[13])
 
-            print("joystick x: ", ControllerData.joystick1[0])
-            print("joystick y: ", ControllerData.joystick1[1])
-            print("joystick1 bool: ", ControllerData.joystick1_click)
-            print("joystick2 x: ", ControllerData.joystick2[0])
-            print("joystick2 y: ", ControllerData.joystick2[1])
-            print("joystick2 click: ", ControllerData.joystick2_click)
-            print("LA: ", ControllerData.LA)
-            print("LB: ", ControllerData.LB)
-            print("RA: ", ControllerData.RA)
-            print("RB: ", ControllerData.RB)
+            # print("joystick x: ", ControllerData.joystick1[0])
+            # print("joystick y: ", ControllerData.joystick1[1])
+            # print("joystick1 bool: ", ControllerData.joystick1_click)
+            # print("joystick2 x: ", ControllerData.joystick2[0])
+            # print("joystick2 y: ", ControllerData.joystick2[1])
+            # print("joystick2 click: ", ControllerData.joystick2_click)
+            # print("LA: ", ControllerData.LA)
+            # print("LB: ", ControllerData.LB)
+            # print("RA: ", ControllerData.RA)
+            # print("RB: ", ControllerData.RB)
 
     @staticmethod
     def normalize_joysticks():
@@ -56,5 +56,9 @@ class ControllerData:
         # TODO: probably should return normalized data instead of changing current data
         ControllerData.joystick1 = (Math.normalize_neg(ControllerData.joystick1[0], 725, 2900),
                                     -Math.normalize_neg(ControllerData.joystick1[1], 0, 2760))
+        ControllerData.joystick2 = (Math.normalize_neg(ControllerData.joystick2[0], 800, 3100),
+                                    -Math.normalize_neg(ControllerData.joystick2[1], 630, 3000))
         print("joystick1 x: ", ControllerData.joystick1[0])
         print("joystick1 y: ", ControllerData.joystick1[1])
+        print("joystick2 x: ", ControllerData.joystick2[0])
+        print("joystick2 y: ", ControllerData.joystick2[1])

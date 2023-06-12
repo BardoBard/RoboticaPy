370a0cfbec1d0a89252cd2f9dff575d5d7455a05
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-12 09:58:15 +0200

    testing

--- Components/Internal/Motors/ArmMotor.py
+++ Components/Internal/Motors/ArmMotor.py
@@ -1,22 +1,19 @@
 import RPi.GPIO as GPIO
-import pyax12.packet
-import serial
-from pyax12 import packet
 from pyax12 import connection
 
 
 class ArmMotor:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
-    GPIO.output(18, GPIO.HIGH)
-
     @staticmethod
     def initialize():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.OUT)
+        GPIO.output(18, GPIO.HIGH)
+
         servo_id = 254
-        serial_connection = connection.Connection(port="/dev/ttyserial1", baudrate=1000000, timeout=3.0)
+        serial_connection = connection.Connection(port="/dev/ttyAMA0", baudrate=1000000, timeout=3.0)
         print("scanning...")
 
-        serial_connection.goto(servo_id, 512, speed=200, degrees=True)
+        serial_connection.goto(servo_id, 45, speed=200, degrees=True)
 
         print("closing")
         serial_connection.close()

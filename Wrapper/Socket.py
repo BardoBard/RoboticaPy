4053982b74df33019d7309697f0295262c020fe0
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-13 16:31:56 +0200

    hopefully fixes the bug

--- Wrapper/Socket.py
+++ Wrapper/Socket.py
@@ -19,20 +19,29 @@ class Socket:
         self.socket = Bluetooth.connect(address, name)
 
     def check_connection(self):
-        """
-        checks connection, keeps trying until connection is established
-        :return: new connection socket
-        """
-        socket_value = object.__getattribute__(self, '__dict__').get('socket')
-        if not Bluetooth.check_connection(socket_value):
-            print("connection lost, trying to reconnect")
-            socket_value = Bluetooth.connect(self.address, self.name)
-        return socket_value
+        return Bluetooth.check_connection(self.socket)
+    
+    def __reconnect_if_needed(self):
+        if self.check_connection(self):
+            return
+        
+        self.socket = Bluetooth.connect(self.address, self.name)
+    
+    # def check_connection(self):
+    #     """
+    #     checks connection, keeps trying until connection is established
+    #     :return: new connection socket
+    #     """
+    #     socket_value = object.__getattribute__(self, '__dict__').get('socket')
+    #     if not Bluetooth.check_connection(socket_value):
+    #         print("connection lost, trying to reconnect")
+    #         socket_value = Bluetooth.connect(self.address, self.name)
+    #     return socket_value
 
-    def __getattribute__(self, name):
-        if name == 'socket':
-            self.socket = self.check_connection()
-        return super().__getattribute__(name)
+    # def __getattribute__(self, name):
+    #     if name == 'socket':
+    #         self.socket = self.check_connection()
+    #     return super().__getattribute__(name)
 
     def __del__(self):
         """
@@ -47,6 +56,8 @@ class Socket:
         :param message: byte array
         :return: void
         """
+        self.__reconnect_if_needed()
+        
         try:
             self.socket.send(message)
         except Exception:
@@ -58,6 +69,7 @@ class Socket:
         :param size: of byte array
         :return: byte array
         """
+        self.__reconnect_if_needed()
         try:
             return self.socket.recv(size)
         except Exception:

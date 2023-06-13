3c4e5d324e0364b2e758ed6c70b3519c68a92463
Kappachino <45740637+K4ppach1no@users.noreply.github.com>
2023-06-13 11:24:41 +0200

    Update Loadcell.py

--- Components/Internal/Loadcell.py
+++ Components/Internal/Loadcell.py
@@ -1,17 +1,41 @@
+import time
 from hx711 import HX711
 
-class CustomHX711:
-    def __init__(self, dout_pin, pd_sck_pin):
+class WeightSensor:
+    def __init__(self, dout_pin, pd_sck_pin, reference_unit=1):
         self.hx = HX711(dout_pin, pd_sck_pin)
+        self.reference_unit = reference_unit
 
-    def setup(self):
-        self.hx.set_reading_format("MSB", "MSB")
-        self.hx.set_reference_unit(1)
-        self.hx.reset()
-        self.hx.tare()
+    def calibrate(self, calibration_factor):
+        """
+        Calibrate the sensor
+        :param (int) calibration_factor: calibration factor
+        :return: void
+        """
+        self.hx.set_scale(calibration_factor)
 
-    def get_weight(self, num_readings=5):
-        return self.hx.get_weight_mean(num_readings)
+    def tare(self, num_samples=10):
+        """
+        Tare the sensor
+        :param (int) num_samples: number of samples
+        :return: void
+        """
+        self.hx.tare(num_samples)
+
+    def get_weight(self):
+        """
+        Get the current weight
+        :return: weight
+        """
+        return self.hx.get_weight(5)
+
+    def get_weight_mean(self, num_samples=5):
+        """
+        Get the current weight mean
+        :param (int) num_samples: number of samples
+        :return: weight mean
+        """
+        return self.hx.get_weight_mean(num_samples)
 
     def power_down(self):
         self.hx.power_down()
@@ -20,17 +44,32 @@ class CustomHX711:
         self.hx.power_up()
 
 
-def main():
-    hx711 = CustomHX711(dout_pin=16, pd_sck_pin=20)
-    hx711.setup()
+if __name__ == '__main__':
+    dout_pin = 16  # DOUT 
+    pd_sck_pin = 20  # PD_SCK 
 
-    try:
-        while True:
-            weight = hx711.get_weight()
-            print(f"Weight: {weight} g")
-    except KeyboardInterrupt:
-        hx711.power_down()
+    weight_sensor = WeightSensor(dout_pin, pd_sck_pin)
 
+    # Calibrate the sensor
+    calibration_factor = 1000  # Adjust this
+    weight_sensor.calibrate(calibration_factor)
 
-if __name__ == "__main__":
-    main()
+    # Tare
+    weight_sensor.tare()
+
+    # Get the current weight
+    weight = weight_sensor.get_weight()
+    print(f"Current weight: {weight} grams")
+
+    # Power down the sensor
+    weight_sensor.power_down()
+
+    # Wait for some time
+    time.sleep(5)
+
+    # Power up the sensor again
+    weight_sensor.power_up()
+
+    # Get the weight after powering up
+    weight = weight_sensor.get_weight()
+    print(f"Weight after powering up: {weight} grams")

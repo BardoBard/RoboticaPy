256570311527a2afcbfe86c45031f2dea7d6055c
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-13 14:29:17 +0200

    removed while loop from socket

--- Wrapper/Socket.py
+++ Wrapper/Socket.py
@@ -24,8 +24,8 @@ class Socket:
         :return: new connection socket
         """
         socket_value = object.__getattribute__(self, '__dict__').get('socket')
-        while not Bluetooth.check_connection(socket_value):
-            print("connection lost, reconnecting soon...")
+        if not Bluetooth.check_connection(socket_value):
+            print("connection lost, trying to reconnect")
             socket_value = Bluetooth.connect(self.address, self.name)
         return socket_value
 

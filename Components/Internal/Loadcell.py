c5657d90cc841519cc54ad04d0a6888e19cd0460
Kappachino <45740637+K4ppach1no@users.noreply.github.com>
2023-06-13 13:26:34 +0200

    Update Loadcell.py

--- Components/Internal/Loadcell.py
+++ Components/Internal/Loadcell.py
@@ -1,22 +1,19 @@
 from hx711 import HX711
 import RPi.GPIO as GPIO
 
-class Loadcell:
-    hx711 = HX711(
-        dout_pin=16,
-        pd_sck_pin=20
-    )
-
-    # This function returns a list of 5 measures
-    def get_weight(self):
-        try:
-            hx711.reset()   # Before we start, reset the HX711 (not obligate)
-            measures = hx711.get_raw_data(num_measures=5)
-        finally:
-            GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
-        return measures
+# This function returns a list of 5 measures
+def get_weight(self):
+    try:
+        hx711 = HX711(
+            dout_pin=16,
+            pd_sck_pin=20
+        )
+        hx711.reset()   # Before we start, reset the HX711 (not obligate)
+        measures = hx711.get_raw_data(num_measures=5)
+    finally:
+        GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+    return measures
 
 if __name__ == '__main__':
-    loadcell = Loadcell()
-    measures = loadcell.get_weight()
+    measures = get_weight()
     print(measures)

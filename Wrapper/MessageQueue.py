02fe490a9080d30de83b5762055472bfee558d9e
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-13 11:06:20 +0200

    moved the bluetooth logic to it's own process that runs independently of the main process

--- Wrapper/MessageQueue.py
+++ Wrapper/MessageQueue.py
@@ -1,6 +1,7 @@
 from multiprocessing import Process, Queue
 from Information.QueueMessage import QueueMessage
 from Information.QueueAgent import QueueAgent
+from typing import List
 
 class MessageQueue:
     def __init__(self):
@@ -11,7 +12,7 @@ class MessageQueue:
         """
         self.__queue.close()
     
-    def get_messages_for(self, queue_agent):
+    def get_messages_for(self, queue_agent) -> List[QueueMessage]:
         """gets all the messages for a queue agent
 
         Args:

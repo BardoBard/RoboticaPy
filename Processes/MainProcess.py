c6b97c8bf818de6d0b682f791c3168d469000d74
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-14 18:24:02 +0200

    idiot here D:

--- Processes/MainProcess.py
+++ Processes/MainProcess.py
@@ -81,8 +81,8 @@ def move_arm(controller_data: ControllerData):
     print(400 if numpy.sign(joystick2[0]) < 0 else 600)
     print(int(numpy.abs(joystick2[0]) * 10))
     print("")
-    rotation_arm.set_speed(int(numpy.abs(joystick2[0]) * 10))
-    rotation_arm.move(450 if numpy.sign(joystick2[0]) < 0 else 550)
+    rotation_arm.set_speed(int(numpy.abs(joystick2[0]) * 100))
+    rotation_arm.move(550 if numpy.sign(joystick2[0]) < 0 else 450)
 
 
 def manual_control(controller_data: ControllerData):

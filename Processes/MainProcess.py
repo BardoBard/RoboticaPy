0788761d309d21188f5cfc179659e920ba6f81d0
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-14 21:47:25 +0200

    I'm so dumb DDDDDD:::????!?!?!!?!?

--- Processes/MainProcess.py
+++ Processes/MainProcess.py
@@ -113,8 +113,8 @@ def manual_arms(controller_data: ControllerData):
     speed = int(numpy.abs(joystick2[0]) * max_speed)
     speed2 = int(numpy.abs(joystick2[1]) * max_speed)
     pos = 400 if numpy.sign(joystick2[0]) < 0 else 600
-    pos2 = 300 if numpy.sign(joystick2[1]) < 0 else 300
-    pos3 = 300 if not numpy.sign(joystick2[1]) < 0 else 300
+    pos2 = 300 if numpy.sign(joystick2[1]) < 0 else 600
+    pos3 = 300 if not numpy.sign(joystick2[1]) < 0 else 600
 
     if speed == 0 or speed2 == 0:
         print("speed 0")

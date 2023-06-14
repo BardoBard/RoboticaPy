31d963a0454aea4c85f2b7d5c2e7fb9e60e04479
Kappachino <45740637+K4ppach1no@users.noreply.github.com>
2023-06-14 13:37:06 +0200

    Update Loadcell.py

--- Components/Internal/Loadcell.py
+++ Components/Internal/Loadcell.py
@@ -1,71 +1,73 @@
-import time
 import sys
-
-referenceUnit = 27
-
 import RPi.GPIO as GPIO
 from hx711 import HX711
+
+class Loadcell:
+    def __init__(self, dout_pin, pd_sck_pin, reference_unit):
+        self.hx = HX711(dout_pin, pd_sck_pin)
+        self.hx.set_reading_format("MSB", "MSB")
+        self.hx.set_reference_unit(reference_unit)
+        self.hx.reset()
+        self.hx.tare()
+        print("Tare done! Add weight now...")
+
+    def get_weight(self):
+        return self.hx.get_weight(5)  # Get the average weight over 5 readings
+
+    def tare(self):
+        self.hx.tare()
+
+    def set_reference_unit(self, reference_unit):
+        self.hx.set_reference_unit(reference_unit)
+
+    def power_down(self):
+        self.hx.power_down()
+
+    def power_up(self):
+        self.hx.power_up()
 
 def cleanAndExit():
     print("Cleaning...")
     GPIO.cleanup()
-        
     print("Bye!")
     sys.exit()
 
-hx = HX711(16, 20)
+def calculate_reference_unit():
+    dout_pin = 16
+    pd_sck_pin = 20
 
-# I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
-# Still need to figure out why does it change.
-# If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
-# There is some code below to debug and log the order of the bits and the bytes.
-# The first parameter is the order in which the bytes are used to build the "long" value.
-# The second paramter is the order of the bits inside each byte.
-# According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
-hx.set_reading_format("MSB", "MSB")
+    # Create an instance of the HX711 class
+    hx = HX711(dout_pin, pd_sck_pin)
 
-# HOW TO CALCULATE THE REFFERENCE UNIT
-# To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
-# In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
-# and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-#hx.set_reference_unit(113)
-hx.set_reference_unit(referenceUnit)
+    # Set the reference unit to an initial value of 1
+    hx.set_reference_unit(1)
 
-hx.reset()
+    # Calibrate the loadcell
+    hx.reset()  # Reset the HX711
+    hx.tare()   # Tare the loadcell 
 
-hx.tare()
+    # Prompt the user to place a known weight on the loadcell
+    input("Place a known weight on the cell and press enter to continue...")
 
-print("Tare done! Add weight now...")
+    # Read the average value from the HX711 over multiple readings
+    average_value = hx.get_raw_data_mean()
 
-# to use both channels, you'll need to tare them both
-#hx.tare_A()
-#hx.tare_B()
+    # Prompt the user to enter the weight of the known object
+    known_weight = float(input("Enter the weight of the known object (in your desired unit): "))
+
+    # Calculate the reference unit
+    reference_unit = average_value / known_weight
+
+    # Print the calculated reference unit
+    print("Reference Unit: {:.2f}".format(reference_unit))
+
+    # Cleanup GPIO pins
+    GPIO.cleanup()
+
 if __name__ == "__main__":
     while True:
         try:
-            # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
-            # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
-            # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment these three lines to see what it prints.
-            
-            # np_arr8_string = hx.get_np_arr8_string()
-            # binary_string = hx.get_binary_string()
-            # print binary_string + " " + np_arr8_string
-            
-            # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
-            val = hx.get_weight(5)
-            print(val)
-    
-            # To get weight from both channels (if you have load cells hooked up 
-            # to both channel A and B), do something like this
-            #val_A = hx.get_weight_A(5)
-            #val_B = hx.get_weight_B(5)
-            #print "A: %s  B: %s" % ( val_A, val_B )
-    
-            hx.power_down()
-            hx.power_up()
-            time.sleep(0.1)
-    
+            # Start calibrating the reference unit
+            calculate_reference_unit()
         except (KeyboardInterrupt, SystemExit):
             cleanAndExit()
-    

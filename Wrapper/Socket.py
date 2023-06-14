9bbdb9c9adbc76a1125b189dbc83e76d1800b7ef
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-14 09:35:50 +0200

    documentation

--- Wrapper/Socket.py
+++ Wrapper/Socket.py
@@ -19,29 +19,21 @@ class Socket:
         self.socket = Bluetooth.connect(address, name)
 
     def check_connection(self):
+        """Checks if the connection is still valid
+
+        Returns:
+            Bool: False if invalid, True if valid
+        """
         return Bluetooth.check_connection(self.socket)
     
     def __reconnect_if_needed(self):
+        """Checks if there is a valid connection 
+        and automatically attempts to reconnect if there isn't
+        """
         if self.check_connection():
             return
         
         self.socket = Bluetooth.connect(self.address, self.name)
-    
-    # def check_connection(self):
-    #     """
-    #     checks connection, keeps trying until connection is established
-    #     :return: new connection socket
-    #     """
-    #     socket_value = object.__getattribute__(self, '__dict__').get('socket')
-    #     if not Bluetooth.check_connection(socket_value):
-    #         print("connection lost, trying to reconnect")
-    #         socket_value = Bluetooth.connect(self.address, self.name)
-    #     return socket_value
-
-    # def __getattribute__(self, name):
-    #     if name == 'socket':
-    #         self.socket = self.check_connection()
-    #     return super().__getattribute__(name)
 
     def __del__(self):
         """

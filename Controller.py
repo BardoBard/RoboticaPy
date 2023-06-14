42b13634da59b2f7d2f49bdf56735a04fd512396
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-14 19:18:36 +0200

    idiot here D:

--- Controller.py
+++ Controller.py
@@ -19,23 +19,29 @@ import time
 
 from Wrapper.Socket import Socket
 
+speed = 50
+original_pos = 512
+pos = 100
+torque = 1023
+cw_limit = 0
+ccw_limit = 1023
+
 
 class Controller:
     if __name__ == '__main__':
-        
         # Setup
         print("Hi B^)")
-        
+
         # queue = MessageQueue()
         # image_process = Process(target=detection_process, args=(queue,))
         # bluetooth_process = Process(target=bluetooth_client_process, args=(queue, ))
-        
+
         # image_process.start()  # start the image detection program
         # print("started image detection process at {}".format(image_process.pid))
-        
+
         # bluetooth_process.start()
         # print("started bluetooth process at {}".format(bluetooth_process.pid))
-        
+
         # Robot logic
         # try:
         #     main_process(queue)
@@ -53,13 +59,17 @@ class Controller:
 
         controller_socket = Socket(controller_mac_address)
 
-        while True:
+        rotation_servo = ArmMotor(2, speed)
+        left_arm1 = ArmMotor(7, speed)
+        right_arm1 = ArmMotor(3, speed)
+        left_arm2 = ArmMotor(10, speed)
+        right_arm2 = ArmMotor(4, speed)
 
-            raw_controller_data = controller_socket.receive(controller_packet_size)
-            controller_data.fill_data(raw_controller_data)
-            joystick2 = controller_data.get_joystick2()
-            time.sleep(0.5)
-            rotation_arm.move(400)
+        rotation_servo.move(original_pos + pos)
+        left_arm1.move(original_pos + pos)
+        right_arm1.move(original_pos - pos)
+        left_arm2.move(original_pos - pos)
+        right_arm2.move(original_pos + pos)
 
         print("killing proccesses")
         # queue.send_kill_message(QueueAgent.CONTROLL, QueueAgent.BLUETOOTH)

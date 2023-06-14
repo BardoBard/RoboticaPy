af322e5b80bf9304c3ceb31d9a3103c0338f7e85
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-14 17:26:58 +0200

    arm motor changes

--- Processes/MainProcess.py
+++ Processes/MainProcess.py
@@ -8,29 +8,27 @@ from Components.Math import Math
 
 import numpy
 
-
+motors = rotation_arm = ArmMotor(2, speed=0),
+left_arm1 = ArmMotor(7, speed=0),
+right_arm1 = ArmMotor(3, speed=0),
+left_arm2 = ArmMotor(10, speed=0),
+right_arm2 = ArmMotor(4, speed=0),
+grabber_Arm = ArmMotor(5, speed=0)
 
 
 def main_process(queue :MessageQueue):
     latest_image_detection = ImageData(None, None, None, None, None, None , None, None, None, False, None)
     latest_controller_data = None
     mode = manual_control
-    
-    # motors = rotation_arm = ArmMotor(2, speed=0),
-    # left_arm1 = ArmMotor(7, speed=0),
-    # right_arm1 = ArmMotor(3, speed=0),
-    # left_arm2 = ArmMotor(10, speed=0),
-    # right_arm2 = ArmMotor(4, speed=0),
-    # grabber_Arm = ArmMotor(5, speed=0)
-    
-        
+
+
     while True:
-        
+
         #get and process messages to this process
         messages = queue.get_messages_for(QueueAgent.CONTROLL)
-        if messages is None: 
+        if messages is None:
             continue
-        
+
         for message in messages:
             print("processing message")
             data = message.get_object()
@@ -47,8 +45,8 @@ def main_process(queue :MessageQueue):
                     return
                 if mode is manual_control:
                     manual_control(latest_controller_data)
-                
-                        
+
+
 def switch_mode(mode, button):
     if button:
         if mode is not manual_control:
@@ -60,7 +58,7 @@ def switch_mode(mode, button):
             return automatic_control
 
 def shutdown_command(controller_data: ControllerData) -> bool:
-    return (controller_data.get_left_b_button() 
+    return (controller_data.get_left_b_button()
             and controller_data.get_right_b_button()
             and controller_data.get_right_a_button())
 
@@ -71,17 +69,19 @@ def control_tracks(controller_data: ControllerData):
     #tracks logic
     joystick1 = controller_data.get_joystick1()
     print("input x: {}, input y: {}".format(joystick1[0], joystick1[1]))
-    
+
     #rotate
     mapped_values = Math.rotate_tuple_over_origin((joystick1[0], joystick1[1]), 45)
     print("left track: {}, right track: {}".format(mapped_values[0], mapped_values[1]))
-    
+
     TrackMotor.move(mapped_values[0], mapped_values[1])
+
+def move_arm(controller_data: ControllerData):
+    joystick2 = controller_data.get_joystick2()
+    rotation_arm.move(300 if numpy.sign(joystick2[0]) < 0 else 1000)
+    rotation_arm.set_speed(numpy.abs(joystick2[0]) * 100)
 
 
 def manual_control(controller_data: ControllerData):
     control_tracks(controller_data)
-    # joystick2 = controller_data.get_joystick2()
-    # rotation_arm.move(300 if numpy.sign(joystick2[0]) < 0 else 1023)
-    # rotation_arm.set_speed(numpy.abs(joystick2[0]) * 100)
-        
+    move_arm(controller_data)

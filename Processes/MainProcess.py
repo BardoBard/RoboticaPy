e768b97488eaed7b74f5c1c5e78b7d6a62c2618a
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-14 18:14:15 +0200

    uhh testing

--- Processes/MainProcess.py
+++ Processes/MainProcess.py
@@ -79,10 +79,10 @@ def control_tracks(controller_data: ControllerData):
 def move_arm(controller_data: ControllerData):
     joystick2 = controller_data.get_joystick2()
     print(400 if numpy.sign(joystick2[0]) < 0 else 600)
-    print(numpy.abs(joystick2[0]) * 30)
+    print(int(numpy.abs(joystick2[0]) * 10))
     print("")
     rotation_arm.move(500 if numpy.sign(joystick2[0]) < 0 else 524)
-    rotation_arm.set_speed(int(numpy.abs(joystick2[0]) * 30))
+    rotation_arm.set_speed(int(numpy.abs(joystick2[0]) * 10))
 
 
 def manual_control(controller_data: ControllerData):

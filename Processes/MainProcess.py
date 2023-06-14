4be745cc42249c45228089ad871449ecd89b16ac
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-14 13:24:13 +0200

    mathed this out real good

--- Processes/MainProcess.py
+++ Processes/MainProcess.py
@@ -62,14 +62,10 @@ def manual_control(controller_data: ControllerData, past_controller_data: Contro
         return
     
     joystick1 = controller_data.get_joystick1()
-    track_input_x = joystick1[0]
-    track_input_y = joystick1[1]
+    print("input x: {}, input y: {}".format(joystick1[0], joystick1[1]))
     
-    print("input x: {}, input y: {}".format(track_input_x, track_input_y))
-    left_motor_val = (track_input_x + track_input_y)/2
-    right_motor_val = (track_input_x - track_input_y)/2
+    mapped_values = Math.rotate_tuple_over_origin(joystick1, 45)
+    print("left track: {}, right track: {}".format(mapped_values[0], mapped_values[1]))
     
-    print("left track: {}, right track: {}".format(left_motor_val, right_motor_val))
-    
-    TrackMotor.move(left_motor_val, right_motor_val)
+    TrackMotor.move(mapped_values[0], mapped_values[1])
         

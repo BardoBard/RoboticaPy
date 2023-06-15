36b9082da1f318540f092163d5561872afedefb1
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-15 11:34:19 +0200

    .

--- Processes/MainProcess.py
+++ Processes/MainProcess.py
@@ -12,20 +12,21 @@ import numpy
 
 max_speed = 50  # TODO: move to class
 offset = -12
-ax12 = Connection(port="/dev/ttyS0", baudrate=1_000_000, timeout=0.4, waiting_time=0.5)
+ax12 = Connection(port="/dev/ttyS0", baudrate=1_000_000)
 
 try:
-    ax12.goto(2, 512, 50, degrees=False)
+    ax12.goto(254, 512, 20, degrees=False)
+    # ax12.goto(2, 512, 50, degrees=False)
     
-    ax12.goto(7, 512, 50, degrees=False)
+    # ax12.goto(7, 512, 50, degrees=False)
 
-    ax12.goto(3, 512, 50, degrees=False)
+    # ax12.goto(3, 512, 50, degrees=False)
 
-    ax12.goto(10, 512, 50, degrees=False)
+    # ax12.goto(10, 512, 50, degrees=False)
 
-    ax12.goto(4, 512, 50, degrees=False)
+    # ax12.goto(4, 512, 50, degrees=False)
 
-    ax12.goto(5, 512, 50, degrees=False)
+    # ax12.goto(5, 512, 50, degrees=False)
 
 except Exception:
     print(traceback.format_exc())

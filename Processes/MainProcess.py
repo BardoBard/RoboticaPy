d069712b8c0551031f28748f7263c4fc4ac01048
BardoBardo <107430862+BardoBard@users.noreply.github.com>
2023-06-15 08:56:20 +0200

    disable tracks

--- Processes/MainProcess.py
+++ Processes/MainProcess.py
@@ -142,11 +142,11 @@ def manual_arms(controller_data: ControllerData):
         print("")
 
     try:
-        # ax12.goto(2, pos, speed, degrees=False)
-        #
-        # ax12.goto(7, pos2, speed2, degrees=False)
-        #
-        # ax12.goto(3, pos3, speed2, degrees=False)
+        ax12.goto(2, pos, speed, degrees=False)
+
+        ax12.goto(7, pos2, speed2, degrees=False)
+
+        ax12.goto(3, pos3, speed2, degrees=False)
 
         ax12.goto(10, pos3, speed2, degrees=False)
 

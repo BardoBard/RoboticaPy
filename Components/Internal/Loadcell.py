108029f9a489553e0b406f5338ea964817121be4
kajatrijsburg <kajatrijsburg@gmail.com>
2023-06-19 13:00:07 +0200

    back to 5

--- Components/Internal/Loadcell.py
+++ Components/Internal/Loadcell.py
@@ -77,7 +77,7 @@ def calculate_reference_unit():
     input("Place a known weight on the cell and press enter to continue...")
 
     # Read the average value from the HX711 over multiple readings
-    average_value = hx.get_weight(10)
+    average_value = hx.get_weight(5)
 
     # Prompt the user to enter the weight of the known object
     known_weight = float(input("Enter the weight of the known object (in your desired unit): "))
